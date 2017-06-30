faf7e999654d662390119714ebdf026490e179eb
Ben Veen <rangerzero003@gmail.com>
2017-06-29 18:38:54 -0700

    Added all the routes that will be need for the app. Set up the layout of the app with mdbootstrap, jquery, and fontawesome. Set up a url param to pass the applicationname:date to the backend so the proper log can be shown.

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ const pouchdb = require('pouchdb');
 let app = express();
 app.use(morgan('combined'));
 app.use(bodyparser.json());
+app.use('/mdb', express.static(__dirname+'/public/mdb/'));
+app.use('/public', express.static(__dirname+'/public'));
+app.set('view engine', 'jade');
 
 /**
- * Need to figure out how to reinitialize couchdb connection on server restart.
- * This is just temporary until a better solution can be found.
- *
+ * Global Server variables
  */
+let configuredServer = true;
+let database_url = '';
+let user_store = '';
+
+let hawkeyedb = new pouchdb(database_url);
 
 
 
@@ -100,7 +106,65 @@ app.route('/log')
   response.send('404');
 });
 
+
+
 app.route('/setup')
+  .all((request, response, next) => {
+    if(configuredServer)
+      response.status('404').send('Server has already been setup');
+}).get((request, response) => {
+    response.render('/public/views/setup.pug');
+}).post((request, response) => {
+    /** TODO store configuration information */
+    configuredServer = true;
+    response.redirect('/');
+});
+
+app.all('*',
+  (request, response, next) => {
+    console.log(configuredServer);
+    if(!configuredServer) {
+      response.redirect('/setup');
+    } else
+      next();
+  }
+);
+
+app.param('applog',
+  (request, response, next, applog) => {
+    console.log(applog);
+    return hawkeyedb.get(applog).then((logdoc) => {
+      request.logdoc = logdoc;
+      return next();
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+);
+
+app.route('/')
+  .get((request, response) => {
+    response.render('/public/views/login.pug');
+}).post((request, response) => {
+
+});
+
+app.all('*',
+  (request, response, next) => {
+    /** TODO ensure user is logged in */
+    next();
+  }
+);
+
+app.route('/dashboard/:applog')
+  .get((request, response) => {
+   response.render('/public/views/dashboard.pug')
+});
+
+app.route('/settings')
+  .get((request, response) => {
+    response.render('/public/views/settings.pug');
+});
 
 
 app.listen(3001, () => {
